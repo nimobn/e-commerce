7c0b005a8ef87afecf7575af333f6b6726bbc505
Afshin Hasani <learn.hasanii@gmail.com>
2021-09-17 19:18:08 +0430

    fix header hight

--- resources/lang/fa/layout.php
+++ resources/lang/fa/layout.php
@@ -2,6 +2,7 @@
 
 return [
     'header' => [
+        'shop_name'     =>  "فروشگاه اینترنتی",
         'login_button'  =>  "ورود به ناحیه کاربری",
         'basket_button' =>  "سبد خرید",
         'search_placeholder' => "دنبال چی ای حاجی؟"
--- resources/views/layouts/shoplayout.blade.php
+++ resources/views/layouts/shoplayout.blade.php
@@ -9,25 +9,25 @@
 <body dir="rtl" class="font-yekan">
     <main class="bg-gray-background w-full h-100">
         <header class="w-full flex shadow-xl bg-white">
-            <div class="w-2/12 bg-gray-background">
-                <div class="w-3/4 mx-auto pt-2">
-                    <img src="{{ asset('images/logo.jpg') }}" alt="" class="w-full">
+            <div class="w-2/12 bg-gray-background pb-3">
+                <div class="w-3/5 mx-auto mt-3">
+                    <a href="" class="no-underline"><img src="{{ asset('images/logo.jpg') }}" alt="" class="w-1/2 mx-auto"></a>
                 </div>
-                <div class="w-3/5 mx-auto pb-2 text-sm">فروشگاه اینترنتی</div>
+                <div class="w-2/5 mx-auto mt-2 text-xs">@lang("layout.header.shop_name")</div>
             </div>
             <div class="w-8/12">
-                <div class="w-6/12 mx-auto mt-8">
+                <div class="w-6/12 mx-auto mt-3">
                      <input type="text" placeholder="@lang("layout.header.search_placeholder")" class="bg-gray-background p-3 rounded-xl w-full text-white"  name="search" id=""> 
                 </div>
             </div>
             <div class="w-2/12 flex flex-wrap content-center">
-                <div class="w-full">
-                    <span class="bg-custom-orange text-white py-3 px-4 rounded-r-lg ml-0 text-xs">icon</span>
-                    <button class="bg-custom-red text-white py-3 px-4 rounded-l-lg mr-0 text-xs">
+                <div class="w-full flex">
+                    <span class="bg-custom-orange text-white py-3 px-4 rounded-r-lg text-xs">icon</span>
+                    <button class="bg-custom-red text-white py-3 px-4 rounded-l-lg text-xs">
                         @lang("layout.header.login_button")
                     </button>
                 </div>
-                <div class="mt-2 w-full">
+                <div class="mt-2 w-full flex">
                     <span class="bg-custom-orange text-white py-3 px-4 rounded-r-lg text-xs">icon</span>
                     <button class="bg-custom-red text-white  py-3 px-4 rounded-l-lg mr-0 text-xs">
                         @lang("layout.header.basket_button") <span class="bg-white rounded-full text-black py-1 px-3">3</span>
