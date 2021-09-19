5dd4a5ec6863da1bd6893e394e3b48b3512f450e
Nima Bidarian <bidarian.n@gmail.com>
2021-09-19 17:25:03 +0430

    slider complete

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+@section('head')
+<link rel="stylesheet" href="{{ asset("css/slider/owl.carousel.min.css") }}">
+<link rel="stylesheet" href="{{ asset("css/slider/owl.theme.green.min.css") }}">
+<link rel="stylesheet" href="{{ asset("css/style.css") }}">
+@endsection
 @section('navigation-menu')
 <div class="absolute bottom-0 pr-10">
     <ul class="flex">
@@ -8,7 +13,7 @@
                 @lang("layout.header.category-title")
                 <i class="fas fa-caret-down"></i>
                 
-                <ul class="absolute hidden text-gray-700 pt-1 group-hover:block w-40 top-12 right-0">
+                <ul class="absolute hidden text-gray-700 pt-1 group-hover:block w-40 top-12 right-0 z-50">
                     <li class="">
                       <a
                         class="rounded-tl bg-white hover:bg-gray-100 hover:text-custom-red py-2 px-4 block whitespace-no-wrap"
@@ -51,10 +56,55 @@
 </div>
 @endsection
 @section('content') 
-
+{{-- Slider --}}
+<div class="sm:flex sm:flex-col md:flex-row hidden justify-around sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto py-10">
+    <div class=" w-2/3 sm:w-full h-full bg-gray-400 rounded-xl overflow-hidden m-1 ">
+        <div class="owl-carousel owl-theme owl-rtl owl-loaded owl-drag relative"> 
+                <img src="{{ asset("images/slider1.png") }}" class="" alt="">
+                <img src="{{ asset("images/slider2.png") }}" class="" alt="">
+        </div>
+    </div>
+    <div class="w-1/3 bg-gray-300 m-1 h-auto rounded-xl overflow-hidden">
+        <img src="{{ asset("images/top-banner.png") }}" class="h-full" alt="">
+    </div>
+</div>
+{{-- End of Slider --}}
 
 @endsection
 
 @section('script')
+<script src="{{ asset("js/jquery-3.6.0.min.js") }}"></script>
+<script src="{{ asset("js/owl.carousel.min.js") }}"></script>
 
+<script>
+    $('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    center:true,
+    nav:true,
+    items:1,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true,
+            dots:true,
+            autoplay:true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true,
+        }
+    }
+})
+$('.owl-prev').html("<i class='far fa-angle-right'></i>");
+$('.owl-next').html("<i class='far fa-angle-left'></i>");
+</script>
 @endsection
